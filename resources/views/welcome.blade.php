689cd30a4f67b6f759c6fb421ba966ffc48104f4
Ashik Rahman <ashrhmn99@gmail.com>
2022-03-10 15:40:32 +0600

    Init project with tailwind setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FMS</title>
+    <link rel="stylesheet" href="../css/app.css">
+</head>
+
+<body>
+    <h1 class="bg-blue-300 text-5xl text-center hover:bg-green-400">Hello World</h1>
+</body>
+
+</html>
